1668451c32155d7efda908a1bd7eb51a56870e05
Martinc94 <martinc94@live.co.uk>
2017-03-02 21:03:18 +0000

    working on two numbers

--- solution.rkt
+++ solution.rkt
@@ -11,6 +11,7 @@
 ;operations
 (define ops '(+ - * /))
 
+
 ;temp values for testing
 (define a 5)
 (define b 25)
@@ -27,15 +28,15 @@
 ;(/ b a)
 
 ;list of all possibilities
-(define l (list (+ a b) (- a b) (* a b) (/ a b) (+ b a) (- b a) (* b a) (/ b a)))
+;(define l (list (+ a b) (- a b) (* a b) (/ a b) (+ b a) (- b a) (* b a) (/ b a)))
 
-(define ll '( (+ a b) (- a b) (* a b) (/ a b) (+ b a) (- b a) (* b a) (/ b a)))
+;(define ll '( (+ a b) (- a b) (* a b) (/ a b) (+ b a) (- b a) (* b a) (/ b a)))
 
 ;show all possibilities
-ll
+;ll
 
 ;show answered list
-l
+;l
 
 ;Method that take in Target nember and list of possible numbers
 (define (solvecount target listNum)
@@ -49,8 +50,73 @@ l
 ;(computeTwo 1 2)
 
 ;test method solveCount
-(solvecount 123 (list 2 2))
-(solvecount 123 (list 3 3))
-(solvecount 123 (list 4 4))
+;(solvecount 123 (list 2 2))
+;(solvecount 123 (list 3 3))
+;(solvecount 123 (list 4 4))
+
+;(permutations '(a b c d + - * /))
+
+;(combinations (list 1 2 3 4 5 6) 2)
+
+; all the possible selections from the original list.
+;(combinations (list 1 2 3 4 5 6))
+
+;(cartesian-product '(+ - * /) '(1 2 3) '(4 5 6))
+
+(define (plus l)
+  (+ (car l)(cadr l)))
+
+;(map plus (combinations (list 1 2 3 4 5 6 )2))
+
+;(map plus (combinations (list  (+ a b) (- a b) (* a b) (/ a b) (+ b a) (- b a) (* b a) (/ b a))2))
+
+;(combinations '(a b + - * / )3)
+;(combinations '(c d + - * / )3)
+;(combinations '(e f + - * / )3)
+
+
+;(cartesian-product '(a b c d) '(b c d) '(+ - * /))
+;(cartesian-product '(a b c d) '(b c d) '(c) '(+ - * /))
+
+;all permutations of a list and an operator
+;(cartesian-product (permutations '(a b)) '(+ - * /))
+
+;gets all possibilities for 2
+(define (allPos l)
+  (cartesian-product (permutations (list (car l) (cadr l))) '(+ - * /)))
+
+(allPos (list 1 2 3 4))
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
